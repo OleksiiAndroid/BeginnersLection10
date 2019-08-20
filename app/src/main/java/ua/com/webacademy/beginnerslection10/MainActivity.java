9405fdbda73819281940de5ae0db35055146cbbf
Oleksii Ponomarov <spinne@ua.fm>
2019-08-20 11:33:23 +0300

    update api 29

--- app/src/main/java/ua/com/webacademy/beginnerslection10/MainActivity.java
+++ app/src/main/java/ua/com/webacademy/beginnerslection10/MainActivity.java
@@ -1,73 +1,228 @@
 package ua.com.webacademy.beginnerslection10;
 
-import android.content.ContentValues;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DataBaseHelper mDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDBHelper = new DataBaseHelper(this);
     }
 
     public void OnClick(View v) {
-        SQLiteDatabase db = mDBHelper.getWritableDatabase();
-
-        Group group;
-        Student student;
+        SharedPreferences preferences;
+        SharedPreferences.Editor editor;
 
         switch (v.getId()) {
             case R.id.button:
-                group = new Group("Number 1");
-                long idGroup = mDBHelper.insertGroup(group);
+                preferences = getPreferences(MODE_PRIVATE);
+                editor = preferences.edit();
 
-                student = new Student(idGroup, "Ivan", "Ivanov", 22);
-                long idStudent = mDBHelper.insertStudent(student);
+                editor.putString("Test", "test");
 
-                Toast.makeText(this, String.format("ID group:%s, ID student:%s", idGroup, idStudent), Toast.LENGTH_LONG).show();
+                editor.commit();
                 break;
             case R.id.button2:
-                student = new Student("Petro", "Petrov", 33);
-                student.id = 1;
+                preferences = getPreferences(MODE_PRIVATE);
+                String text1 = preferences.getString("Test", "");
 
-                int uCount = mDBHelper.updateStudent(student);
-                Toast.makeText(this, String.format("Updated %s students", uCount), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, text1, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
-                ArrayList<Student> students = mDBHelper.getStudents();
-                Toast.makeText(this, String.format("Students:%s", students.size()), Toast.LENGTH_LONG).show();
+                preferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+                editor = preferences.edit();
+
+                editor.putString("Test", "test2");
+
+                editor.commit();
                 break;
             case R.id.button4:
-                student = mDBHelper.getStudent(1);
+                preferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+                String text3 = preferences.getString("Test", "");
 
-                if (student == null) {
-                    Toast.makeText(this, "Student not found", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(this, student.FirstName, Toast.LENGTH_LONG).show();
-                }
+                Toast.makeText(this, text3, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
-                int dCount = mDBHelper.deleteStudent(1);
-                Toast.makeText(this, String.format("Deleted %s students", dCount), Toast.LENGTH_LONG).show();
-                break;
-            case R.id.button7:
-                Intent intent = new Intent(this, Activity2.class);
+                Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.button7:
+                preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+                String text4 = preferences.getString("pref2", "");
+                Toast.makeText(this, text4, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button8:
+                saveInternalFile("MyFile1.txt", "File data");
+                break;
+            case R.id.button9:
+                String text9 = readInternalFile("MyFile1.txt");
+                Toast.makeText(this, text9, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button10:
+                if (hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File folder = Environment.getExternalStorageDirectory();
+                        folder = new File(folder.getAbsolutePath() + "/MyFolder");
+
+                        saveExternalFile(folder, "MyFile2.txt", "File data");
+                    }
+                } else {
+                    Toast.makeText(this, "You don't have permission for this", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.button11:
+                if (hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File folder = Environment.getExternalStorageDirectory();
+                        folder = new File(folder.getAbsolutePath() + "/MyFolder");
+
+                        if (folder.exists()) {
+                            String text11 = readExternalFile(folder, "MyFile2.txt");
+                            Toast.makeText(this, text11, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "You don't have permission for this", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.button12:
+                Student student = new Student("Ivan", "Ivanov", 22);
+
+                Gson gson = new GsonBuilder().create();
+                String json = gson.toJson(student);
+
+                saveInternalFile("Student.txt", json);
+                break;
+            case R.id.button13:
+                String json2 = readInternalFile("Student.txt");
+
+                Gson gson2 = new GsonBuilder().create();
+                Student student2 = gson2.fromJson(json2, Student.class);
+
+                Toast.makeText(this, student2.FirstName, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button14:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    String[] permissions = {
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    };
+
+                    requestPermissions(permissions, 0);
+                } else {
+                    Toast.makeText(this, "You can request permissions only in Android 6+", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+
+        return true;
+    }
+
+    private void saveInternalFile(String fileName, String data) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE)));
+
+            writer.write(data);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readInternalFile(String fileName) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            reader.close();
+
+            return builder.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void saveExternalFile(File folder, String fileName, String data) {
+        try {
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            File file = new File(folder, fileName);
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8));
+
+            writer.write(data);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readExternalFile(File folder, String fileName) {
+        File file = new File(folder, fileName);
+
+        try {
+            if (file.exists()) {
+                StringBuilder builder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+                reader.close();
+
+                return builder.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
--- app/src/main/java/ua/com/webacademy/beginnerslection10/Student.java
+++ app/src/main/java/ua/com/webacademy/beginnerslection10/Student.java
@@ -1,34 +1,21 @@
 package ua.com.webacademy.beginnerslection10;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Student {
-    public static final String TABLE_NAME = "Students";
-
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_ID_GROUP = "_idGroup";
-    public static final String COLUMN_FIRST_NAME = "FirstName";
-    public static final String COLUMN_LAST_NAME = "LastName";
-    public static final String COLUMN_AGE = "Age";
-
-    public long id;
-    public long idGroup;
-
+    @SerializedName("FirstName")
     public String FirstName;
-    public String LastName;
-    public long Age;
 
-    public Student() {
-    }
+    @SerializedName("LastName")
+    public String LastName;
+
+    @SerializedName("Age")
+    public int Age;
 
     public Student(String firstName, String lastName, int age) {
         this.FirstName = firstName;
         this.LastName = lastName;
         this.Age = age;
     }
-
-    public Student(long idGroup, String firstName, String lastName, int age) {
-        this.idGroup = idGroup;
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.Age = age;
-    }
 }
+
